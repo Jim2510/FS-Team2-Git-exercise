a95a1dfaa450d1c9624c12f7114694323a4fe123
Jim2510 <gmguglielmino@gmail.com>
2024-01-06 10:15:44 +0100

    Code Review
    Reviewing and cleaning code, correcting errors, adding comments

--- PJ-Team2/team.js
+++ PJ-Team2/team.js
@@ -1,127 +1,121 @@
+// Dichiarazione array students
+
 const students = [{
-    name: "Gabriele",
-    surname: "Nicotra",
-    age: 23,
-    city: "Catania",
-    hobby: "Cinema",
-    favoriteFood: "Pizza",
-    favoriteVideoGame: "Call of Duty",
-    favoriteFilm: "A clockwork orange",
-    favoriteBook: "1984",
-    petName: ""
-
-
-},
-{
-    name: "Felice",
-    surname: "Curcetti",
-    age: 33,
-    city: "Foggia",
-    hobby: "Cinema",
-    favoriteFood: "Sushi",
-    favoriteVideoGame: "",
-    favoriteFilm: "",
-    favoriteBook: "",
-    petName: ""
-
-
-},
-{
-    name: "Gianmarco",
-    surname: "Guglielmino",
-    age: 29,
-    city: "Lodi",
-    hobby: "Music Producer",
-    favoriteFood: "Italian food",
-    favoriteVideoGame: "Counter Strike",
-    favoriteFilm: "Casino",
-    favoriteBook: "LOTR",
-    petName: "Walter"
-
-
-},
-{
-    name: "David",
-    surname: "Bodrug",
-    age: 22,
-    city: "Milano",
-    hobby: "Gym",
-    favoriteFood: "Sushi",
-    favoriteVideoGame: "COD",
-    favoriteFilm: "Joker",
-    favoriteBook: "you & me",
-    petName: "Pollon"
-
-
-},
-
+        name: "Gabriele",
+        surname: "Nicotra",
+        age: 23,
+        city: "Catania",
+        hobby: "Cinema",
+        favoriteFood: "Pizza",
+        favoriteVideoGame: "Call of Duty",
+        favoriteFilm: "A clockwork orange",
+        favoriteBook: "1984",
+        petName: ""
+    },
+    {
+        name: "Felice",
+        surname: "Curcetti",
+        age: 33,
+        city: "Foggia",
+        hobby: "Cinema",
+        favoriteFood: "Sushi",
+        favoriteVideoGame: "",
+        favoriteFilm: "",
+        favoriteBook: "",
+        petName: ""
+    },
+    {
+        name: "Gianmarco",
+        surname: "Guglielmino",
+        age: 29,
+        city: "Lodi",
+        hobby: "Music Producer",
+        favoriteFood: "Italian food",
+        favoriteVideoGame: "Counter Strike",
+        favoriteFilm: "Casino",
+        favoriteBook: "LOTR",
+        petName: "Walter"
+    },
+    {
+        name: "David",
+        surname: "Bodrug",
+        age: 22,
+        city: "Milano",
+        hobby: "Gym",
+        favoriteFood: "Sushi",
+        favoriteVideoGame: "COD",
+        favoriteFilm: "Joker",
+        favoriteBook: "you & me",
+        petName: "Pollon"
+    }
 ]
 
-function nome(student) {
 
-    student.sort(function (a, b) {
-        if (a.name < b.name) {
-            return -1;
-        }
-        if (a.name > b.name) {
-            return 1;
-        }
-        return 0;
-    }).forEach(element => console.log(element.name))
+// Funzione che prende come parametro un array e cicla gli elementi riordinati in ordine alfabetico tramite il metodo sort
 
-    student.sort(function (a, b) {
+function orderStudents(student) {
+    student.sort((a, b) => {
         if (a.surname < b.surname) {
             return -1;
-        }
-        if (a.surname > b.surname) {
+        } else {
             return 1;
         }
-        return 0;
-    }).forEach(element => console.log(element.surname))
-
+    }).forEach(element => console.log(`${element.surname} ${element.name}`))
 }
-nome(students)
 
-//utilizzato metodo 'sort()' per mettere in ordine alfabetico comparandole proprieta 'name' e 'surname' dell oggetto e il metodo 'forEac()' per iterare l array .
+// FELICE
 
-// - Print the team in age order (name age).
-function task2(students) {
-    return students.sort((student, student2) => student.age - student2.age)
-        .forEach((student) => console.log(student.name, student.age))
 
-}
-task2(students)
+
+
 /*Creo una funzione che mi ritorna un array ordinato per età crescente(utilizzando il metodo sort che compara le proprietà age degli oggetti).
 Tramite il metodo foreach mi stampo il nome e l'età degli oggetti*/
 
+function orderAge(students) {
+    return students.sort((student, student2) => student.age - student2.age)
+        .forEach((student) => console.log(student.name, student.age))
+}
+
+// GABRIELE
+
+
+
+// Utilizzo metodo map per restituire un array contentente solo le proprietà ages alla quale viene applicato il sort per mettere in ordine
+// gli elementi e viene quindi calcolato l'indice corrispondente all'età centrale. 
+// se l'array ha una lunghezza pari, verrà presa l'età a metà tra i due valori centrali tramite il metodo find.
 
 function calculateMiddleAge(students) {
+    const ages = students.map(student => student.age).sort((a, b) => a - b)
+    const middleIndex = Math.floor(ages.length / 2)
+    const middleAge = ages[middleIndex]
+    const middleAgedStudent = students.find(student => student.age === middleAge)
+    console.log(`Middle age student: ${middleAgedStudent.name} - ${middleAgedStudent.age}`)
+}
 
-    // utilizzo del metodo map per ritornare un nuovo array con le proprietà age degli oggetti ordinate.
-    const ages = students.map(student => student.age).sort((a, b) => a - b);
-    // indice età intermedia
-    const middleIndex = Math.floor(ages.length / 2);
-    // applicazione di middleIndex sull'array mappato
-    const middleAge = ages[middleIndex];
-  
-    // metodo find che restituisce solo un elemento che rispecchia la condizione che restituisce l'emento desiderato comparandolo con la
-    // variabile middleAge
-    const middleAgedStudent = students.find(student => student.age === middleAge);
-    console.log(`Middle age student: ${middleAgedStudent.name} - ${middleAgedStudent.age}`);
-  }
-  
+// GIANMARCO
 
-  // chiamata della funzione
-  calculateMiddleAge(students);
 
-  
-  function petName(students) {
 
- students.forEach(element => { 
-    if(element.petName !== "") {
-    console.log(element.name + ": " + element.petName) }
+// funzione che cicla gli elementi dell'array e valuta se la proprietà petName non è una stringa vuota e ne ritorna il valore della proprietà
 
-    
-});
-  }
-  petName(students)
+function petName(students) {
+    students.forEach(element => {
+        if (element.petName !== "") {
+            console.log(element.name + ": " + element.petName)
+        }
+    });
+}
+
+// DAVID
+
+
+
+
+// Chiamata delle funzioni
+console.log('Students in alphabetical order: ')
+orderStudents(students)
+console.log('Students in age order: ')
+orderAge(students)
+calculateMiddleAge(students)
+console.log('Students with pets: ')
+petName(students)
